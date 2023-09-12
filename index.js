d27b9080c0ac9590e16e99528850e8423148e7db
Gabriel Coelho <gabrielbr100@outlook.com>
2023-09-12 12:48:52 -0300

    .

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 import express from "express";
 import eventsRouter from "./Router/eventsRouter.js";
 import cidadeRouter from "./Router/cidadeRouter.js";
+import cors from "cors";
 
 const server = express();
-server.use((req, res, next) => {
-  console.log("Origem da solicitação:", req.get("origin"));
-
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-
-  next();
-});
+server.use(cors({ origin: "*" }));
 //configurar a aplicação para aceitar objetos aninhados.
 server.use(express.urlencoded({ extended: true }));
 
 //configurar a aplicação para processar corretamente o formado json.
 server.use(express.json());
 
-server.use("/events", eventsRouter);
-server.use("/city", cidadeRouter);
+server.use("/aluno18-pfsii/events", eventsRouter);
+server.use("/aluno18-pfsii/city", cidadeRouter);
 
 const localName = "localhost";
 const serverName = "0.0.0.0";
