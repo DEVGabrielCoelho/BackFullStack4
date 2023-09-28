7393827b24be20b40146beef27f5d2dc87f84343
Gabriel Coelho - Telematica <gabriel.coelho@telematica.com.br>
2023-09-28 15:56:13 -0300

    Teste

--- Persistence/eventsBD.js
+++ Persistence/eventsBD.js
@@ -79,7 +79,7 @@ export default class EventsBD {
   async consultSimple(term) {
     const connection = await Connect();
     const sql =
-      "SELECT e.title, e.setTime, e.startDate, e.endDate, c.cidade AS city_code, e.description FROM events e JOIN cidade c ON s.city_code = c.cidade";
+      "SELECT e.title, e.setTime, e.startDate, e.endDate, c.cidade AS city_code, e.description FROM events e INNER JOIN cidade c  ON e.city_code = c.cidade";
     const values = ["%" + term + "%"];
     global.poolConnections.pool.releaseConnection(connection);
     const [rows] = await connection.query(sql, values);
